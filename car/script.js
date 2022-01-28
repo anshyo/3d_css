c0827c07adac2310a75c9dd9306305f8d2f3402c
Ansh Bhardwaj <93650767+anshyo@users.noreply.github.com>
2022-01-28 13:36:40 +0530

    a

--- car/script.js
+++ car/script.js
@@ -1,1 +1,14 @@
-const car=
+const car=document.querySelector('.car');
+const btn1=document.querySelector('.r-1');
+const btn2=document.querySelector('.r-2');
+const btn3=document.querySelector('.r-3');
+
+btn1.addEventListener('click', () => {
+    car.classList.toggle('carrotatetop');
+})
+btn2.addEventListener('click', () => {
+    car.classList.toggle('carrotateleft');
+})
+btn3.addEventListener('click', () => {
+    car.classList.toggle('carrotatelefttop');
+})
